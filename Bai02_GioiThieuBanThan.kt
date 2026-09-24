eb0c46063e63c9378a42ebb694fe8e00661d684c
Danh-Amings <nguyenngocdanh1701@gmail.com>
2026-09-24 13:25:45 +0700

    Buoi02  Bai02_GioiThieuBanThan

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,10 @@
+// Họ tên: Nguyễn Ngọc Danh MSSV: 25810010
+
+fun main() {
+    val fullName = "Nguyen Ngoc Danh"
+    val namSinh = 2005
+    val namHienTai = 2026
+    val age = namHienTai - namSinh
+    
+    println ("Chào thầy, em tên là $fullName, sinh năm $namSinh, hiện tại $age tuổi")
+}
