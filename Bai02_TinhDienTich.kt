90dc0af10b6f4fe795387a7e5fba459d8538708f
Danh-Amings <nguyenngocdanh1701@gmail.com>
2026-09-26 13:25:38 +0700

    Buoi03 Bai02 _Tinh dien tich

---
+++ Bai02_TinhDienTich.kt
@@ -0,0 +1,12 @@
+// Họ tên: Nguyễn Ngọc Danh MSSV: 25810010
+fun tinhDT(chieuDai: Double, chieuRong: Double): Double {
+    return chieuDai * chieuRong
+}
+
+val dienTich1 = tinhDT(5.0, 3.0)
+val dienTich2 = tinhDT(7.2, 4.5)
+ 
+fun main() {
+    println("Dien tich hinh chu nhat 1: $dienTich1")
+    println("Dien tich hinh chu nhat 2: $dienTich2")
+}
